47e8e4965234e90fb8a9c82958af9ad9ba87560c
Ayodele Opetumo <ayodele.opetumo@dealdey.com>
2018-11-02 14:30:50 +0100

    Implement authentication guards for lazy loaded health modules

--- src/app/health/health.module.ts
+++ src/app/health/health.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from '../auth/shared/guards/auth.guard';
+
 const routes: Routes = [
-  { path: 'meals', loadChildren: './meals/meals.module#MealsModule' },
+  {
+    path: 'meals',
+    canActivate: [AuthGuard],
+    loadChildren: './meals/meals.module#MealsModule'
+  },
   {
     path: 'schedule',
+    canActivate: [AuthGuard],
     loadChildren: './schedule/schedule.module#ScheduleModule'
   },
   {
     path: 'workouts',
+    canActivate: [AuthGuard],
     loadChildren: './workouts/workouts.module#WorkoutsModule'
   }
 ];
