8b29f426fd5151462db9be75998543b49bddcbf5
Ayodele Opetumo <ayodele.opetumo@dealdey.com>
2018-11-02 21:20:22 +0100

    Create component architecture and implement reactive form to add meal

--- src/app/health/meals/containers/meal/meal.component.ts
+++ src/app/health/meals/containers/meal/meal.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Meal } from '../../../shared/models/meal.interface';
+
 @Component({
   selector: 'app-meal',
   styleUrls: ['meal.component.scss'],
   template: `
     <div class="meal">
-      I am a meal!
+      <div class="meal__title">
+        <h1>
+          <img src="assets/food.svg" alt="food">
+          <span>Create meal</span>
+        </h1>
+      </div>
+
+      <div>
+        <app-meal-form (create)="addMeal($event)"></app-meal-form>
+      </div>
     </div>
   `
 })
@@ -13,4 +24,8 @@ export class MealComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  addMeal(meal: Meal) {
+    console.log(meal);
+  }
 }
