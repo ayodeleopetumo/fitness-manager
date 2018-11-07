ee9a2c4b986930cba71de869611494b6927740f5
Ayodele Opetumo <ayodele.opetumo@dealdey.com>
2018-11-07 13:08:24 +0100

    Add date switching feature to switch dates based on start of week

--- src/app/health/schedule/components/schedule-calendar/schedule-calendar.component.ts
+++ src/app/health/schedule/components/schedule-calendar/schedule-calendar.component.ts
@@ -1,21 +1,44 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-schedule-calendar',
   styleUrls: ['schedule-calendar.component.scss'],
   template: `
     <div class="calendar">
-      {{ date | json }}
+      <app-schedule-controls
+        [selected]="selectedDay"
+        (move)="onChange($event)">
+      </app-schedule-controls>
     </div>
   `
 })
-export class ScheduleCalendarComponent implements OnInit, OnDestroy {
+export class ScheduleCalendarComponent {
+  selectedDay: Date;
+
   @Input()
-  date: Date;
+  set date(date: Date) {
+    this.selectedDay = new Date(date.getTime());
+  }
+
+  @Output()
+  change = new EventEmitter<Date>();
 
   constructor() {}
 
-  ngOnInit() {}
+  private getStartOfWeek(date: Date) {
+    const day = date.getDay();
+    const diff = date.getDate() - day + (day === 0 ? -6 : 1);
+    return new Date(date.setDate(diff));
+  }
 
-  ngOnDestroy() {}
+  onChange(weekOffset: number) {
+    const startOfWeek = this.getStartOfWeek(new Date());
+    const startDate = new Date(
+      startOfWeek.getFullYear(),
+      startOfWeek.getMonth(),
+      startOfWeek.getDay()
+    );
+    startDate.setDate(startDate.getDay() + weekOffset * 7);
+    this.change.emit(startDate);
+  }
 }
